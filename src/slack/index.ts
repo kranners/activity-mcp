9f73e0f4d7b540ec34699cdd9a655189b21095a5
Aaron Pierce <aaron@cute.engineer>
2025-05-23 22:56:43 +1000

    adding slack search

---
+++ src/slack/index.ts
@@ -0,0 +1,29 @@
+import 'dotenv/config';
+import { WebClient } from '@slack/web-api';
+
+const token = process.env.SLACK_USER_TOKEN;
+const web = new WebClient(token);
+
+export const getUserId = async () => {
+  const identity = await web.auth.test();
+
+  if (identity.user_id === undefined) {
+    throw new Error(`Must be using a user token. Given identity: ${JSON.stringify(identity)}`);
+  }
+
+  return identity.user_id;
+}
+
+type MessagesFromUserInput = {
+  userId: string;
+  before: string;
+  after: string;
+}
+
+export const getMessagesFromUser = async ({ userId, before, after }: MessagesFromUserInput) => {
+  return web.search.messages({
+    query: `from:@${userId} before:${before} after:${after}`,
+    sort: "timestamp",
+    count: 20,
+  });
+}
