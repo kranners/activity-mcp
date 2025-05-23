4b501e206026a1ee601c480c6b64007ff6c7b14f
Aaron Pierce <aaron@cute.engineer>
2025-05-23 21:59:44 +1000

    dropping github for now, moving on to slack

---
+++ README.md
@@ -0,0 +1,21 @@
+# activity-mcp
+
+MCP server for retrieving recent activity across a number of platforms.
+
+Primarily, this is for automating timesheets.
+
+## `TODO`
+
+GitHub
+- [ ] PRs
+- [ ] Commits
+- [ ] Reviews
+
+Slack
+- [ ] Recent messages
+- [ ] Messages in threads
+- [ ] DMs?
+
+Timing
+- [ ] 
+
---
+++ src/github/index.ts
@@ -0,0 +1,83 @@
+import { graphql } from "@octokit/graphql";
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const { GITHUB_TOKEN } = process.env;
+
+const graphqlWithAuth = graphql.defaults({
+  headers: {
+    authorization: `token ${GITHUB_TOKEN}`,
+  },
+});
+
+// Define the expected result type
+interface ViewerLoginResult {
+  viewer: {
+    login: string;
+    repositories: {
+      nodes: { name: string }[];
+    };
+  };
+}
+
+const query = `
+  query activity($username: String!, $from: DateTime!, $to: DateTime!) {
+    user(login: $username) {
+      contributionsCollection(from: $from, to: $to) {
+        commitContributionsByRepository(maxRepositories: 100) {
+          repository {
+            name
+            owner {
+              login
+            }
+          }
+        }
+        pullRequestContributionsByRepository(maxRepositories: 100) {
+          repository {
+            nameWithOwner
+          }
+          contributions(first: 50) {
+            nodes {
+              occurredAt
+              pullRequest {
+                title
+                url
+                body
+                closed
+                closedAt
+                updatedAt
+              }
+            }
+          }
+        }
+        pullRequestReviewContributionsByRepository(maxRepositories: 100) {
+          repository {
+            nameWithOwner
+          }
+          contributions(first: 50) {
+            nodes {
+              occurredAt
+              pullRequestReview {
+                pullRequest {
+                  title
+                  url
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+async function main() {
+  const result = await graphqlWithAuth<ViewerLoginResult>(query, {
+    num: 5,
+  });
+
+  console.log(JSON.stringify(result, null, 2));
+}
+
+main(); 
