6ee523b95c6d719f608c947f959fde53888b640d
Aaron Pierce <aaron@cute.engineer>
2025-06-03 09:27:31 +1000

    feat(slack): optimising slack tools

--- src/slack/index.ts
+++ src/slack/index.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import { WebClient } from "@slack/web-api";
+import { z } from "zod";
 
 const token = process.env.SLACK_USER_TOKEN;
 const web = new WebClient(token);
@@ -16,20 +17,117 @@ export const getUserId = async () => {
   return identity.user_id;
 };
 
+const Channel = z.object({
+  id: z.string(),
+  name: z.string(),
+});
+
+const Message = z.object({
+  channel: Channel,
+  iid: z.string(),
+  text: z.string(),
+  user: z.string().nullable(),
+  username: z.string().nullable(),
+});
+
+const SearchResponse = z.object({
+  messages: z.object({
+    matches: Message.array(),
+    paging: z.object({
+      count: z.number(),
+      page: z.number(),
+      pages: z.number(),
+      total: z.number(),
+    }),
+  }),
+  ok: z.boolean(),
+  query: z.string(),
+});
+
 type MessagesFromUserInput = {
-  before: string;
-  after: string;
+  dayBeforeRange?: string;
+  dayAfterRange?: string;
+  page: number;
+  channelNames?: string[];
+  userIds?: string[];
+};
+
+type DayRange = Pick<MessagesFromUserInput, "dayBeforeRange" | "dayAfterRange">;
+
+const buildBeforeAfterFilter = ({
+  dayBeforeRange,
+  dayAfterRange,
+}: DayRange): string | undefined => {
+  if (dayBeforeRange === undefined || dayAfterRange === undefined) {
+    return;
+  }
+
+  return `before:${dayAfterRange} after:${dayBeforeRange}`;
 };
 
 export const getMessagesFromUser = async ({
-  before,
-  after,
+  dayBeforeRange,
+  dayAfterRange,
+  page,
+  channelNames = [],
+  userIds = [],
 }: MessagesFromUserInput) => {
-  const userId = await getUserId();
-
-  return web.search.messages({
-    query: `from:@${userId} before:${before} after:${after}`,
-    sort: "timestamp",
-    count: 20,
+  const channelFilter = channelNames.map((name) => `in:#${name}`).join(" ");
+  const userFilter = userIds.map((id) => `from:${id}`).join(" ");
+  const beforeAfterFilter = buildBeforeAfterFilter({
+    dayBeforeRange,
+    dayAfterRange,
   });
+
+  const query = `${beforeAfterFilter} ${channelFilter}${userFilter}`;
+
+  const response = await web.search.messages({
+    query,
+    sort: "timestamp",
+    count: 50,
+    page,
+  });
+
+  return SearchResponse.parse(response);
+};
+
+const Conversation = z.object({
+  id: z.string(),
+  name: z.string(),
+  created: z.number(),
+  is_archived: z.boolean(),
+  updated: z.number(),
+  topic: z.object({
+    value: z.string(),
+  }),
+});
+
+type GetAllConversationsInput = {
+  includeArchived: boolean;
+  includeDirectMessages: boolean;
+  includePublicChannels: boolean;
+  includePrivateChannels: boolean;
+};
+
+const buildChannelTypesQuery = ({
+  includeDirectMessages,
+  includePublicChannels,
+  includePrivateChannels,
+}: GetAllConversationsInput) => {
+  const types: string[] = [];
+
+  if (includeDirectMessages) types.push("im&mpim");
+  if (includePublicChannels) types.push("public_channel");
+  if (includePrivateChannels) types.push("private_channel");
+
+  return types.join(",");
+};
+
+export const getAllChannels = async (params: GetAllConversationsInput) => {
+  const conversations = await web.conversations.list({
+    exclude_archived: !params.includeArchived,
+    types: buildChannelTypesQuery(params),
+  });
+
+  return Conversation.parse(conversations);
 };
