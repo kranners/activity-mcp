3cd8c0d571cfa067908c47861fd901a38540f5b0
Aaron Pierce <aaron@cute.engineer>
2025-06-14 19:50:35 +1000

    adding end-to-end tests with mcp-use

--- test_e2e.py
+++ test_e2e.py
@@ -2,6 +2,7 @@ import pytest
 import pytest_asyncio
 import logging
 from dotenv import load_dotenv
+from langchain_ollama import ChatOllama
 from langchain_openai import ChatOpenAI
 from mcp_use import MCPAgent, MCPClient
 
@@ -20,11 +21,29 @@ async def agent():
             }
         }
     )
-    llm = ChatOpenAI(model="gpt-4o")
+    logging.info("Loaded MCP client")
+
+    # llm = ChatOllama(
+    #     model="qwen3:4b",
+    #     temperature=0.8,
+    # )
+
+    llm = ChatOpenAI()
+    logging.info("Loaded LLM")
+
     agent = MCPAgent(llm=llm, client=client, max_steps=30)
+    logging.info("Loaded agent, starting!")
 
     yield agent
     await agent.close()
+
+
+@pytest_asyncio.fixture(scope="function", autouse=True)
+def reset_agent(agent):
+    agent.clear_conversation_history()
+    logging.info("Cleared conversation history.")
+
+    return agent
 
 
 def format_prompt_result(prompt, result):
@@ -37,41 +56,29 @@ def format_prompt_result(prompt, result):
     logging.info(formatted)
 
 
+EASY_PROMPTS = [
+    "Tell me my ClickUp user ID and username.",
+    "Tell me my username on GitHub.",
+    "Tell me a random one of my git repositories.",
+    "Tell me my user ID on Harvest.",
+    "Tell me someone's email from my Google directory.",
+    "Tell me a color from my Google calendar.",
+    "Tell me the current time as an ISO timestamp.",
+    "Tell me my name and user ID on Slack.",
+]
+
+
+@pytest.mark.parametrize("prompt", EASY_PROMPTS)
 @pytest.mark.asyncio(loop_scope="session")
-async def test_current_time_is_iso_timestamp(agent):
-    prompt = "Tell me the current time as an ISO timestamp."
-    result = await agent.run(prompt)
+async def test_easy_prompts(agent, prompt):
+    result = await agent.run(f"""
+        I am a Tool Tester. I only say correct information from running tools.
+        If the required tools don't work for whatever reason, I will say the failure state
+        and the word "FAILED".
+
+        My task: {prompt}
+    """)
 
     format_prompt_result(prompt, result)
 
-    assert "unable" not in result
-
-
-@pytest.mark.asyncio(loop_scope="session")
-async def test_slack_messages(agent):
-    prompt = "What is my name and user ID on Slack?"
-    result = await agent.run(prompt)
-
-    format_prompt_result(prompt, result)
-
-    assert "unable" not in result
-
-
-@pytest.mark.asyncio(loop_scope="session")
-async def test_clickup_user(agent):
-    prompt = "What is my ClickUp user ID?"
-    result = await agent.run(prompt)
-
-    format_prompt_result(prompt, result)
-
-    assert "unable" not in result
-
-
-@pytest.mark.asyncio(loop_scope="session")
-async def test_google_calendar(agent):
-    prompt = "What is the Google event color ID closest to purple?"
-    result = await agent.run(prompt)
-
-    format_prompt_result(prompt, result)
-
-    assert "unable" not in result
+    assert "FAILED" not in result
