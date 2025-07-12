351db770b02b16cd2009b6ced9f9e7eaa4a128c3
Aaron Pierce <aaron@cute.engineer>
2025-07-12 16:55:54 +1000

    adding shadcn button, prettying up the main screen

--- packages/desktop-app/src/App.tsx
+++ packages/desktop-app/src/App.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { ElectronAPI } from "@/electron/preload";
 import React, { useEffect, useState } from "react";
 
@@ -22,13 +23,17 @@ function App() {
   });
 
   return (
-    <div className="flex flex-row justify-center items-center h-screen w-screen bg-red-200">
-      <h1>Vite + React</h1>
-      <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <button onClick={sendPing}>ping</button>
+    <div className="flex flex-col justify-center items-center h-screen w-screen">
+      <div className="p-4 flex flex-col rounded-lg shadow-lg gap-8">
+        <h1 className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance">
+          Vite + React
+        </h1>
+        <div className="flex gap-4">
+          <Button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </Button>
+          <Button onClick={sendPing}>ping</Button>
+        </div>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
